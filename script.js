8bf4d283ffc2cba2577ab639a24984eb2f284e4f
Mark Anthony Patrick Rodriguez <markrodriguez003@gmail.com>
2020-05-01 23:39:33 -0400

    Added complete layout for site. Still missing core JS.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-/* ***************** HTML VIEW ***************** */
-{/* <div class="container time-block mb-4 "> // main block page
+$(document).ready(function () {
+    /* ***************** HTML VIEW ***************** */
+    {/* <div class="container time-block mb-4 "> // main block page
 
         <div class="row">
             <div class="col-1 hour"> 10AM </div> <!-- TIME COL -->
@@ -9,39 +10,51 @@
 
 </div> */}
 
-console.log(moment()); //REFERENCE
-var timeHrs = moment().format("ha"); // SPITS OUT CURRENT HOUR
-var hrs = ["12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm", "12am", 
-"1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am"];
- 
-var mainContainer = $(".mainBlockPage");
-var mainRow = $("<div></div>").addClass("row");
- 
-mainContainer.append(mainRow);
 
-var timeBlockDiv = $("<div></div>");
-var dayEntryDiv = $("<input>");
-var saveBtnDiv = $("<button>");
-var brDiv = $("<br>");
+    console.log(moment()); //REFERENCE
+    var timeHrs = moment().format("ha"); // SPITS OUT CURRENT HOUR
+    var hrs = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm",
+        "7pm", "8pm", "9pm", "10pm", "11pm", "12am", "1am", "2am", "3am", "4am", "5am"];
 
-timeBlockDiv.addClass("col-1 hour");
-dayEntryDiv.addClass("col-10 past"); // USE ATTR to set present future and past class
-saveBtnDiv.addClass("col-1 btn saveBtn");
+    var mainContainer = $(".mainBlockPage"); // Selects main page
+    var blockRow = $("<div></div>").addClass("row"); // Create empty time block row + gives it class
 
 
-/* mainRow.append(timeBlockDiv.text(hrs[0]), dayEntryDiv, saveBtnDiv); */
+    var timeBlockDiv = $("<div></div>").addClass("col-1 hour");      // Creates L Time 
+    var dayEntryDiv = $("<input>").addClass("col-10 past");         // Creates M hour data entry
+    var saveBtnDiv = $("<button>").addClass("col-1 btn saveBtn");  //  Creates R save button    
+
+    /* 
+    blockRow.append(timeBlockDiv.text(hrs[0]), dayEntryDiv, saveBtnDiv); */
+
+    const DAY_HOURS = 23;
+
+    var blockObject;
+
+    for (var i = 1; i <= DAY_HOURS; i++) {
+        /*  blockObject =`
+             <div class="row">
+                 <div class="col-1 hour"> ${hrs[i]} </div> <!-- TIME COL -->
+                 <input class="col-10 past "> <!-- DAY ENTRY COL -->
+                 <button class="col-1 btn  saveBtn"></button> <!-- SAVE BTN -->
+             </div>`
+         mainContainer.append(blockObject); */
+     
+        mainContainer.append(blockRow);
+        blockRow.append(timeBlockDiv.text(hrs[i]));
+        blockRow.append(dayEntryDiv);
+        blockRow.append(saveBtnDiv);
+
+
+    }
 
 
 
 
-/* const DAY_HOURS = 23;
-for (var i = 1 ; i <= DAY_HOURS; i++){
-    mainRow.append(timeBlockDiv.text(hrs[i]));
-    mainRow.append(dayEntryDiv);
-    mainRow.append(saveBtnDiv);
-    mainContainer.after($("<div> </div>"));
-    mainContainer.after(brDiv);
-    
-    
- } */
 
+
+
+
+
+
+});
